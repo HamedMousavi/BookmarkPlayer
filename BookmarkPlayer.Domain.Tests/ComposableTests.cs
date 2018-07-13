41df51a856cea7da6a996f3091ea22d1129904a0
Hamed <HamedMosavi@gmail.com>
2018-07-13 20:06:43 +0430

    Add Count. Generalize naming.

---
+++ BookmarkPlayer.Domain.Tests/ComposableTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+
+namespace BookmarkPlayer.Domain.Tests
+{
+
+    public class ComposableTests
+    {
+
+        [Fact]
+        public void ComposableShouldReportItsChildrenCount()
+        {
+            var trainings = new Composable("Trainings");
+            Assert.Equal(0, trainings.Count());
+
+            trainings.Add(new Composable("Akka .Net"));
+            Assert.Equal(1, trainings.Count());
+
+            trainings.Add(new Composable("Advanced TDD"));
+            Assert.Equal(2, trainings.Count());
+
+            var series = new Composable("Series")
+            {
+                new Composable("Friends"),
+                new Composable("Family Guy"),
+                new Composable("IT Crowd"),
+            };
+
+            Assert.Equal(3, series.Count());
+        }
+    }
+}
---
+++ BookmarkPlayer.Domain/Composable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace BookmarkPlayer.Domain
+{
+
+    public class Composable : IEnumerable<Composable>, IComparable<Composable>
+    {
+
+        public string Title { get; }
+
+
+        public Composable(string title)
+        {
+            Title = title;
+        }
+
+
+        public void Add(Composable media)
+        {
+            if (_children == null) _children = new SortedSet<Composable>();
+            _children.Add(media);
+        }
+
+
+        public int Count()
+        {
+            return _children == null ? 0 : _children.Count;
+        }
+
+
+        public IEnumerator<Composable> GetEnumerator()
+        {
+            return _children?.GetEnumerator();
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _children?.GetEnumerator();
+        }
+
+
+        public int CompareTo(Composable other)
+        {
+            return string.Compare(
+                Title,
+                other.Title,
+                StringComparison.InvariantCultureIgnoreCase);
+        }
+
+
+        private SortedSet<Composable> _children;
+    }
+}
